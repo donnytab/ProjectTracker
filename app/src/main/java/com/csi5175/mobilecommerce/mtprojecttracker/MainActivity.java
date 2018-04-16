88082da6e21e1faa93c594e50f926dfab2fe2a7c
downson <downson.yuen@gmail.com>
2018-04-16 10:24:26 -0400

    First commit

---
+++ app/src/main/java/com/csi5175/mobilecommerce/mtprojecttracker/MainActivity.java
@@ -0,0 +1,52 @@
+package com.csi5175.mobilecommerce.mtprojecttracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.mobile.auth.core.IdentityHandler;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+import com.amazonaws.mobile.config.AWSConfiguration;
+
+public class MainActivity extends AppCompatActivity {
+    private AWSCredentialsProvider awsCredentialsProvider;
+    private AWSConfiguration awsConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+
+                // Obtain the reference to the AWSCredentialsProvider and AWSConfiguration objects
+                awsCredentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
+                awsConfiguration = AWSMobileClient.getInstance().getConfiguration();
+
+                // Use IdentityManager#getUserID to fetch the identity id.
+                IdentityManager.getDefaultIdentityManager().getUserID(new IdentityHandler() {
+                    @Override
+                    public void onIdentityId(String identityId) {
+                        Log.d("YourMainActivity", "Identity ID = " + identityId);
+
+                        // Use IdentityManager#getCachedUserID to
+                        //  fetch the locally cached identity id.
+                        final String cachedIdentityId =
+                                IdentityManager.getDefaultIdentityManager().getCachedUserID();
+                    }
+
+                    @Override
+                    public void handleError(Exception exception) {
+                        Log.d("YourMainActivity", "Error in retrieving the identity" + exception);
+                    }
+                });
+            }
+        }).execute();
+    }
+}
