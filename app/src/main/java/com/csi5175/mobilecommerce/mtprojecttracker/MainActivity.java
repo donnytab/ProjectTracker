e7c8de0b47edb5a8b59f21805b3d7a824286ebd9
downson <downson.yuen@gmail.com>
2018-04-16 19:00:59 -0400

    Implement tab 2 & tab 3

--- app/src/main/java/com/csi5175/mobilecommerce/mtprojecttracker/MainActivity.java
+++ app/src/main/java/com/csi5175/mobilecommerce/mtprojecttracker/MainActivity.java
@@ -29,13 +29,18 @@ import java.util.List;
 
 public class MainActivity extends TabActivity implements TabHost.OnTabChangeListener {
     private TabHost tabHost;
-    private int currentTab;
     private ListView listView1;
+    private ListView listView2;
+    private ListView listView3;
 
     private static final String TODO_SPEC = "todoSpec";
     private static final String COMPLETED_SPEC = "completedSpec";
     private static final String ALL_SPEC = "allSpec";
     private static final String LIST1_TAG = "TO DO";
+    private static final String LIST2_TAG = "COMPLETED";
+    private static final String LIST3_TAG = "ALL";
+
+    private int currentTab;
 
     private AWSCredentialsProvider awsCredentialsProvider;
     private AWSConfiguration awsConfiguration;
@@ -45,14 +50,24 @@ public class MainActivity extends TabActivity implements TabHost.OnTabChangeList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        currentTab = 2;
+
         listView1 = (ListView)findViewById(R.id.list1);
+        listView2 = (ListView)findViewById(R.id.list2);
+        listView3 = (ListView)findViewById(R.id.list3);
         tabHost = getTabHost();
         tabHost.setOnTabChangedListener(this);
 
 
         TabSpec tabSpec1 = tabHost.newTabSpec(TODO_SPEC);
+        TabSpec tabSpec2 = tabHost.newTabSpec(COMPLETED_SPEC);
+        TabSpec tabSpec3 = tabHost.newTabSpec(ALL_SPEC);
+
 
         tabSpec1.setIndicator(LIST1_TAG);
+        tabSpec2.setIndicator(LIST2_TAG);
+        tabSpec3.setIndicator(LIST3_TAG);
+
         tabSpec1.setContent(new TabHost.TabContentFactory() {
             @Override
             public View createTabContent(String s) {
@@ -60,14 +75,43 @@ public class MainActivity extends TabActivity implements TabHost.OnTabChangeList
             }
         });
 
-        List<String> list = new ArrayList<String>();
-        list.add("MT 1");
-        list.add("MT 2");
-        list.add("MT 3");
+        tabSpec2.setContent(new TabHost.TabContentFactory() {
+            @Override
+            public View createTabContent(String s) {
+                return listView2;
+            }
+        });
 
-        listView1.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list));
+        tabSpec3.setContent(new TabHost.TabContentFactory() {
+            @Override
+            public View createTabContent(String s) {
+                return listView3;
+            }
+        });
+
+        List<String> list1 = new ArrayList<String>();
+        list1.add("MT 1");
+        list1.add("MT 2");
+        list1.add("MT 3");
+
+        List<String> list2 = new ArrayList<String>();
+        list2.add("DONNY 1");
+        list2.add("DONNY 2");
+
+        List<String> list3 = new ArrayList<String>();
+        list3.add("Jade 1");
+
+        listView1.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list1));
+        listView2.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list2));
+        listView3.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list3));
 
         tabHost.addTab(tabSpec1);
+        tabHost.addTab(tabSpec2);
+        tabHost.addTab(tabSpec3);
+
+        tabHost.setCurrentTab(currentTab);
+
+
 
         AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
             @Override
